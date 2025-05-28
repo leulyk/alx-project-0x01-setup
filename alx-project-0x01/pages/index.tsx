e41a41360abc92a58490e592583ae62ff53f415f
Leul Yiheyis <leulykpro@gmail.com>
2025-05-28 23:00:05 +0300

    fix linter issue

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/layout/Header";
+import Header from "@/components/layout/Header"
 
 const Home: React.FC = () => {
   return (
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
             Welcome to our Application!
           </h1>
           <p className="mt-4 text-xl text-white">
-            We're glad you're here. Explore and enjoy your experience.
+            We&apos;re glad you&apos;re here. Explore and enjoy your experience.
           </p>
           <button className="mt-6 px-6 py-3 bg-white text-blue-500 rounded-full font-semibold hover:bg-gray-200 transition">
             Get Started
